4c2bd2254007c01b923a8b5ba8219ea81f1fd25d
Maryam <souzanmaryam@gmail.com>
2024-12-14 20:26:13 +0000

    Margin and purchase cost

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,5 +1,12 @@
 select
-    *
+    products_id
+    ,date_date
+    ,orders_id
+    ,revenue
+    ,quantity
+    ,purchase_price
+    ,ROUND((sa.quantity*pr.purchase_price),2) AS purchase_cost
+    ,ROUND(sa.revenue-(sa.quantity*pr.purchase_price),2) AS margin
 FROM {{ ref("stg_raw__sales") }} sa
-LEFT JOIN {{ ref("stg_raw__product") }} pa
+LEFT JOIN {{ ref("stg_raw__product") }} pr
 USING (products_id)
