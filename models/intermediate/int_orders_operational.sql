633b30f184b3fd89416f5ddd04a643b5b781cd61
Maryam <souzanmaryam@gmail.com>
2024-12-15 12:17:07 +0000

    edit operational orders

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,12 +1,16 @@
 SELECT
-orders_id
-,date_date
-,margin
-,shipping_fee
-,ship_cost
-,logcost
-,ROUND(margin+shipping_fee-logcost-ship_cost ,2) AS operational_margin
-FROM {{ ref('int_orders_margin') }} orma
-LEFT JOIN {{ ref('stg_raw__ship') }}ship
+om.orders_id
+,om.date_date
+,om.margin
+,om.quantity
+,om.revenue
+,om.purchase_cost
+,om.margin
+,sh.shipping_fee
+,sh.ship_cost
+,sh.logcost
+,ROUND(om.margin+sh.shipping_fee-sh.logcost-sh.ship_cost ,2) AS operational_margin
+FROM {{ ref('int_orders_margin') }} om
+LEFT JOIN {{ ref('stg_raw__ship') }} sh
 USING (orders_id)
 ORDER BY orders_id DESC
